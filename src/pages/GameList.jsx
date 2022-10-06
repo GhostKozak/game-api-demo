d6ac8fada7dcaa6e5768299689d9b6587fc50b9a
Gökhan Tanrıverdi <codeodd3@gmail.com>
2022-10-06 17:52:53 +0300

    Edit getGames

--- src/pages/GameList.jsx
+++ src/pages/GameList.jsx
@@ -1,10 +1,10 @@
 import React,{useState} from 'react';
-import axios from "axios";
 import { useEffect } from 'react';
 import {getGames} from "../services/api/data"
+import {getPlatformImage,colorizeMetaCritic} from "../util";
+import { AiFillTags } from 'react-icons/ai'
 
 const GameList = () => {
-    const [apiData, setApiData] = useState(null);
     const [games, setGames] = useState(null);
     const data = getGames();
     
@@ -12,26 +12,51 @@ const GameList = () => {
         setGames(data);
     }, [data])
 
+    //Functions 
     return(
         <div className='p-5'>
             <h1 className='mb-4'>Game List</h1>
-            <div className='grid grid-cols-3 gap-4'>
+            <div className='container mx-auto grid grid-cols-4 gap-4'>
             {
-                (games && games.map(game => (
-                    <div className='border p-4 rounded mb-3' key={game.id}>
-                        <img src={game.background_image} alt="" className='max-h-80 mx-auto w-full aspect-square object-cover object-top' />
-                        <h1>{game.name}</h1>
-                        <p> { game.genres.map(genre => <span key={genre.id}>{genre.name}, </span>) } </p>
-                        <p>Metacritic : {game.metacritic}/100</p>
-                        <p>Platforms: { game.platforms.map(plat => <span key={plat.platform.id}>{plat.platform.name}, </span>) }</p>
-                        <p>Release Date : {game.released}</p>
-                        <div className='grid grid-cols-4 gap-2 max-w-full'>
+                (games && games.map(game => (                    
+                    <div className='w-full h-[440px] overflow-hidden relative shadow-lg' key={game.id}>
+                        <div className='relative h-full before:opacity-60 before:bg-black before:w-full before:h-full before:absolute before:top-0 before:left-0 before:z-10'>
+                            <img src={game.background_image} alt="" className='w-full h-full mx-auto aspect-square object-cover object-top' />
+                        </div>
+                        <div className='absolute left-0 bottom-0 px-8 py-10 transition-all text-white z-20 w-full'>
+                            <h1 className='font-black uppercase'>{game.name}</h1>
+                            <div className='flex w-full'>
+                                <p className='flex items-baseline gap-1'> <AiFillTags /> 
+                                { 
+
+                                    game.genres.map((genre, index) => {
+                                        if (index < 3) {
+                                            return (<span className='text-base capitalize' key={genre.id}>{genre.name}</span>)
+                                        } else {
+                                            return;
+                                        }
+                                    }) 
+                                    
+                                }
+                                </p>
+                                <p className='flex gap-2 ml-auto'>
+                                    { 
+                                        game.platforms
+                                            .filter(plat => (plat.platform.id === 1 || plat.platform.id === 4 || plat.platform.id === 187))
+                                            .sort((a,b) => a.platform.id > b.platform.id)
+                                            .map(plat => <span className='flex items-end' key={plat.platform.id}>{ getPlatformImage(plat.platform.id) } </span>)
+                                    }
+                                </p>
+                            </div>
+                        </div>
+                        <div className={`p-15 absolute right-2 top-2 z-20 ${ colorizeMetaCritic(game.metacritic) }`} >{game.metacritic} / 100</div>
+                        {/* <div className='grid grid-cols-4 gap-2 max-w-full'>
                             {
                                 game.short_screenshots.map(ss => (
                                     <img key={ss.id} className='rounded' src={ss.image} alt="" />
                                 ))
                             }
-                        </div>
+                        </div> */}
                     </div>
                 )))
             }
---
+++ src/util.jsx
@@ -0,0 +1,44 @@
+import { FiMonitor } from 'react-icons/fi';
+import { 
+    SiPlaystation4,
+    SiPlaystation,
+    SiNintendoswitch,
+    SiXbox,
+    SiApple,
+    SiLinux
+} from 'react-icons/si';
+import { RiEarthLine } from 'react-icons/ri'
+
+export const getPlatformImage = (platformID) => {
+    switch (platformID){
+        case 4: //PC
+            return <FiMonitor />;
+        case 187: //PS5
+            return <SiPlaystation />;
+        case 1: //Xbox One
+            return <SiXbox />;
+        case 18: //PS4
+            return <SiPlaystation />;
+        case 7: //Switch Nintendo
+            return <SiNintendoswitch />;
+        case 5: //MacOS
+            return <SiApple />;
+        case 6: //Linux
+            return <SiLinux />;
+        default:
+            return <RiEarthLine />;
+    }
+}
+
+
+export const colorizeMetaCritic = (metaNote) => {
+    if(metaNote === null){
+        return "text-white"
+    }else if(metaNote < 25){
+        return "text-red-700"
+    }else if (metaNote < 50){
+        return "text-yellow-400"
+    }else if (metaNote <= 100) {
+        return "text-lime-500"
+    }
+}
